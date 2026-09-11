d9f3999430f455a2f1e0012a01c0cb5b82eb48aa
RANA ARIFIN <108522512+085871373603@users.noreply.github.com>
2026-09-11 14:15:10 +0700

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='arinex-chat-final-v1';
+const CACHE='arinex-chat-final-v1.1';
 const ASSETS=['./','./index.html','./css/style.css','./js/app.js','./manifest.webmanifest','./img/chat_logo.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
